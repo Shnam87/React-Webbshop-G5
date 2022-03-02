d6fdb134b227a059da7d107fb5118af0d7de1da2
[Simon] <[simon.sandevik@outlook.com]>
2022-03-02 23:39:21 +0100

    fixade recommendations

--- webbshop/src/components/Recomendation.js
+++ webbshop/src/components/Recomendation.js
@@ -1,20 +1,48 @@
 import React from "react";
 import styled from "styled-components";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function Recomendation({ item }) {
   return (
     <div>
-      <Link to={`/product/${item.id}`}><Img src={item.url} alt="" /></Link>
+      <Container to={`/product/${item.id}`}>
+        <Img src={item.url} alt="" />
+        <Title>{item.title}</Title>
+        <Price>{item.price} Kr</Price>
+      </Container>
     </div>
   );
 }
 
-
 const Img = styled.img`
   width: 10em;
-  border-radius:0.5em;
-  background-color:white;
+  grid-row: 1 / span 2;
+  grid-column: 1;
+  margin:auto;
+`;
+
+const Container = styled(Link)`
+  display: grid;
+  border-radius: 0.5em;
+  background-color: white;
+  text-decoration: none;
+  height:7em;
+`;
+
+const Title = styled.div`
+  grid-column: 2;
+  grid-row: 1;
+  padding-inline: 1em;
+  padding-top: 1em;
+
+  color: black;
+`;
+
+const Price = styled.div`
+  grid-column: 2;
+  grid-row: 2;
+  padding-inline: 1em;
+  color: black;
 `;
 
 export default Recomendation;
