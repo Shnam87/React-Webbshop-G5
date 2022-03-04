5f6ee4edf58eade8e994b98d9dfe163219ab8db4
[Simon] <[simon.sandevik@outlook.com]>
2022-03-04 12:25:33 +0100

    initial checkout

--- webbshop/src/App.js
+++ webbshop/src/App.js
@@ -1,22 +1,53 @@
-import './App.css';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import Header from './components/Header';
-import Nav from './components/Nav';
-import Products from './pages/Products';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import Products from "./pages/Products";
+import Checkout from "./pages/Checkout";
+import { useState } from "react";
 
 function App() {
+  const [cart, setCart] = useState([
+    {
+      id: 234967,
+      title: "Bugatti (On Sale)",
+      description:
+        "Quo neque error repudiandae fuga? Ipsa laudantium molestias eos \n sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam\n recusandae alias error harum maxime adipisci amet laborum.",
+      price: 550,
+      storage: 21,
+      quantity:2,
+      url: "https://cdn.pixabay.com/photo/2012/05/29/00/43/car-49278_1280.jpg",
+    },
+    {
+      id: 678453,
+      title: "Ford Mustang",
+      description:
+        "Quo neque error repudiandae fuga? Ipsa laudantium molestias eos \n sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam\n recusandae alias error harum maxime adipisci amet laborum.",
+      price: 1899000,
+      storage: 55,
+      quantity:1,
+      url: "https://cdn.pixabay.com/photo/2013/07/12/12/56/ford-mustang-146580_1280.png",
+    }
+  ]);
+  
+const removeItem = (id)=>{
+  console.log(cart.filter(e=> e.id == id))
+  
+}
+  
+
   return (
     <div className="App">
       <BrowserRouter>
-      <Header />
-      <Nav />
+        <Header />
+        <Nav />
 
-      <Routes>
-        <Route path="/" element={<Products />} />
-        <Route />
-        <Route />
-        <Route />
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Products />} />
+          <Route />
+          <Route path="/checkout" element={<Checkout cart={cart} removeItem={removeItem}/>} />
+          <Route />
+        </Routes>
       </BrowserRouter>
     </div>
   );
--- webbshop/src/pages/Checkout.js
+++ webbshop/src/pages/Checkout.js
@@ -1,9 +1,51 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
-function Checkout() {
+function Checkout({ cart, removeItem }) {
+ 
+ const handleDelete = (e)=>{
+  removeItem(e)
+ }
+
+  let sum = 0;
+  cart.map((c) => (sum += c.price * c.quantity));
+
   return (
-    <div>Checkout</div>
-  )
+    <div>
+      {cart != "" ? (
+        <div>
+          <table>
+            <tbody>
+              <tr>
+                <th>Product</th>
+                <th>Amount</th>
+                <th>Quant.</th>
+                <th>Id</th>
+              </tr>
+
+              {cart.map((c) => (
+                <tr key={c.id}>
+                  <td>{c.title}</td>
+                  <td>{c.price}</td>
+                  <td>{c.quantity}</td>
+                  <td>{c.id}</td>
+                  <td><button onClick={handleDelete}>Remove</button></td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <div>sum: {sum} kr</div>
+        </div>
+      ) : (
+        <div>
+          Your cart is empty. To get more items,{" "}
+          <Link to="/">
+            <button>Click Here</button>
+          </Link>
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default Checkout
+export default Checkout;
